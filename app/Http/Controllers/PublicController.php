79c208fadf2fb4d06f18dc5b87c311734949fcc8
Ladhari <amin.ladhari@gmail.com>
2020-03-30 20:03:03 +0100

    fix user verification error

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -138,7 +138,7 @@ class PublicController extends Controller
             $user = User::find($id);
             if (!empty($request['code'])) {
                 if ($request['code'] === $user->verification_code) {
-                  $user->user_verified = 1;
+                    $user->user_verified = 0;
                     $user->verification_code = null;
                     $user->save();
                     $json['type'] = 'success';
@@ -953,7 +953,7 @@ class PublicController extends Controller
             return $json;
         }
     }
-      public function getArticles()
+    public function getArticles()
     {
         $json = array();
         $articles = Article::get()->toArray();
