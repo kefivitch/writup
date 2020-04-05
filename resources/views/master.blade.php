a85a3b2d25fb487dd0823bbc775829b685537677
Ladhari <amin.ladhari@gmail.com>
2020-04-05 14:52:08 +0100

    fix lang issue

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -72,8 +72,8 @@
 	</script>
 	@endif
 	<script>
-		window.trans = <? php
-		$lang_files = File:: files(resource_path(). '/lang/'.App:: getLocale());
+		window.trans = <?php
+		$lang_files = File::files(resource_path(). '/lang/'.App:: getLocale());
 		$trans = [];
 		foreach($lang_files as $f) {
 			$filename = pathinfo($f)['filename'];
